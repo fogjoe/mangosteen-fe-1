227adb671514192a0bc913e903245c01479887de
liweize <1055824818@qq.com>
2022-11-18 23:12:00 +0800

    定义 DEBUG

--- vite.config.ts
+++ vite.config.ts
@@ -8,45 +8,55 @@ import { VantResolver } from 'unplugin-vue-components/resolvers'
 import { svgstore } from './src/vite_plugins/svgstore'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id: any) {
-          if (id.includes('echarts')) {
-            return 'echarts'
+export default defineConfig(({ command }) => {
+  return {
+    define:
+      command === 'build'
+        ? {
+            DEBUG: false
           }
-          if (id.includes('mock') || id.includes('faker')) {
-            return 'mock'
-          }
-          if (id.includes('vant')) {
-            return 'vant'
-          }
-          if (id.includes('node_modules')) {
-            return 'vendor'
+        : {
+            DEBUG: true
+          },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id: any) {
+            if (id.includes('echarts')) {
+              return 'echarts'
+            }
+            if (id.includes('mock') || id.includes('faker')) {
+              return 'mock'
+            }
+            if (id.includes('vant')) {
+              return 'vant'
+            }
+            if (id.includes('node_modules')) {
+              return 'vendor'
+            }
           }
         }
       }
-    }
-  },
-  plugins: [
-    vue(),
-    vueJsx({
-      transformOn: true,
-      mergeProps: true
-    }),
-    svgstore(),
-    Components({
-      resolvers: [VantResolver()]
-    })
-  ],
-  server: {
-    // host: "0.0.0.0",
-    // port: 3000,
-    // disableHostCheck: true,
-    proxy: {
-      '/api/v1': {
-        target: 'http://119.13.81.176:3000/'
+    },
+    plugins: [
+      vue(),
+      vueJsx({
+        transformOn: true,
+        mergeProps: true
+      }),
+      svgstore(),
+      Components({
+        resolvers: [VantResolver()]
+      })
+    ],
+    server: {
+      // host: "0.0.0.0",
+      // port: 3000,
+      // disableHostCheck: true,
+      proxy: {
+        '/api/v1': {
+          target: 'http://119.13.81.176:3000/'
+        }
       }
     }
   }
